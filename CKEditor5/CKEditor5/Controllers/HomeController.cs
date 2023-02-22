10b9d35ec836ba0eae255e57ef0d4b5886d41c5a
hungho <hung.ho@tpisoftware.com>
2023-02-22 16:18:18 +0800

    CKEditor5 範例程式

---
+++ CKEditor5/CKEditor5/Controllers/HomeController.cs
@@ -0,0 +1,78 @@
+﻿using CKEditor5.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using System.IO;
+namespace CKEditor5.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+        [HttpPost]
+        public async Task<JsonResult> ImageTestAsync(IFormFile Files)
+        {
+
+            var SaveDir = "D:\\HRPORTAL\\Attachment\\QAQuestion";
+            FileInfo _FileInfo = new FileInfo(Files.FileName);
+            var FileExt = _FileInfo.Extension;
+            var newID = $"{_FileInfo.Name}";
+
+            SaveFile(Files, SaveDir!, newID);
+
+            //若ret=-1即上傳失敗，直接跳出迴圈
+           
+            return Json(200);
+        }
+
+        public static int SaveFile(IFormFile FormFile, string SaveDir, string NewFileName = "")
+        {
+            Stream _Stream = null!;
+            try
+            {
+                if (FormFile.Length > 0)
+                {
+                    var DirPath = (NewFileName == string.Empty) ? Path.Combine(SaveDir, FormFile.FileName) : Path.Combine(SaveDir, NewFileName);
+
+                    if (!Directory.Exists(SaveDir))
+                        Directory.CreateDirectory(SaveDir);
+
+                    using (_Stream = System.IO.File.Create(DirPath))
+                        FormFile.CopyTo(_Stream);
+
+                    return 1;
+                }
+                return -1;
+            }
+            catch (Exception ex)
+            {
+                return -1;
+            }
+            finally
+            {
+                if (_Stream != null)
+                    _Stream.Close();
+            }
+        }
+    }
+}
---
+++ CKEditor5/CKEditor5/wwwroot/js/site.js
@@ -0,0 +1,179 @@
+﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your JavaScript code.
+
+window.addEventListener("load", (e) => {
+
+	ClassicEditor
+		.create(document.querySelector('.editor'), {
+			extraPlugins: [MyCustomUploadAdapterPlugin],
+			toolbar: {
+				items: [
+					'heading', '|', 'bold', 'italic', 'link', 'bulletedList', 'numberedList', '|',
+					'alignment', 'outdent', 'indent', '|', 'fontSize', 'fontColor', '|',
+					'imageUpload', 'blockQuote', 'insertTable', 'mediaEmbed', 'undo', 'redo'
+				]
+			},
+			language: 'en',
+			image: {
+				toolbar: [
+					'imageTextAlternative',
+					'imageStyle:inline',
+					'imageStyle:block',
+					'imageStyle:side'
+				]
+			},
+			table: {
+				contentToolbar: [
+					'tableColumn',
+					'tableRow',
+					'mergeTableCells'
+				]
+			},
+			licenseKey: '',
+
+
+		})
+		.then(editor => {
+			window.editor = editor;
+		})
+		.catch(error => {
+			console.error('Oops, something went wrong!');
+			console.error(error);
+		});
+});
+
+
+
+class MyUploadAdapter {
+	constructor(loader) {
+		// The file loader instance to use during the upload.
+		this.loader = loader;
+	}
+
+	// Starts the upload process.
+	upload() {
+		var reader = new FileReader();
+
+		return new Promise((resolve, reject) => {
+			reader.addEventListener('load', () => {
+				resolve({ default: reader.result });
+			});
+
+			reader.addEventListener('error', err => {
+				reject(err);
+			});
+
+			reader.addEventListener('abort', () => {
+				reject();
+			});
+
+			this.loader.file.then(file => {
+				reader.readAsDataURL(file);
+			});
+		})
+	}
+
+	// Aborts the upload process.
+	abort() {
+		if (this.xhr) {
+			this.xhr.abort();
+		}
+	}
+
+	// Initializes the XMLHttpRequest object using the URL passed to the constructor.
+	_initRequest() {
+		const xhr = (this.xhr = new XMLHttpRequest());
+
+		// Note that your request may look different. It is up to you and your editor
+		// integration to choose the right communication channel. This example uses
+		// a POST request with JSON as a data structure but your configuration
+		// could be different.
+		xhr.open("POST", "/Home/ImageTest", true);
+		xhr.responseType = "json";
+	}
+
+	// Initializes XMLHttpRequest listeners.
+	_initListeners(resolve, reject, file) {
+		const xhr = this.xhr;
+		const loader = this.loader;
+		const genericErrorText = `無法上傳檔案: ${file.name}.`;
+
+		xhr.addEventListener("error", () => reject(genericErrorText));
+		xhr.addEventListener("abort", () => reject());
+		xhr.addEventListener("load", () => {
+			const response = xhr.response;
+
+			console.log('response', response);
+
+			// This example assumes the XHR server's "response" object will come with
+			// an "error" which has its own "message" that can be passed to reject()
+			// in the upload promise.
+			//
+			// Your integration may handle upload errors in a different way so make sure
+			// it is done properly. The reject() function must be called when the upload fails.
+			if (!response || response.error) {
+				return reject(response && response.error ? response.error.message : genericErrorText);
+			}
+
+			// If the upload is successful, resolve the upload promise with an object containing
+			// at least the "default" URL, pointing to the image on the server.
+			// This URL will be used to display the image in the content. Learn more in the
+			// UploadAdapter#upload documentation.
+			resolve({
+				default: response.url,
+			});
+		});
+
+		// Upload progress when it is supported. The file loader has the #uploadTotal and #uploaded
+		// properties which are used e.g. to display the upload progress bar in the editor
+		// user interface.
+		if (xhr.upload) {
+			xhr.upload.addEventListener("progress", evt => {
+				if (evt.lengthComputable) {
+					loader.uploadTotal = evt.total;
+					loader.uploaded = evt.loaded;
+				}
+			});
+		}
+	}
+
+	// Prepares the data and sends the request.
+	_sendRequest(file) {
+		// Prepare the form data.
+		const data = new FormData();
+
+		data.append("Files", file);
+
+		console.log('file:', file);
+
+		// Important note: This is the right place to implement security mechanisms
+		// like authentication and CSRF protection. For instance, you can use
+		// XMLHttpRequest.setRequestHeader() to set the request headers containing
+		// the CSRF token generated earlier by your application.
+
+		// Send the request.
+		this.xhr.send(data);
+	}
+}
+
+// ...
+
+function MyCustomUploadAdapterPlugin(editor) {
+	editor.plugins.get("FileRepository").createUploadAdapter = loader => {
+		// Configure the URL to the upload script in your back-end here!
+		return new MyUploadAdapter(loader);
+	};
+}
+
+function getDataFromTheEditor() {
+	return window.editor.getData();
+}
+
+document.getElementById('getdata').addEventListener('click', () => {
+	alert(getDataFromTheEditor());
+
+	//document.getElementById('test').value = getDataFromTheEditor();
+	
+});
